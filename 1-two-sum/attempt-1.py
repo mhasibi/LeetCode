68f8ab68c57256de29e56a8520fc54db0a2c054c
mhasibi <mojan.hasibi@gmail.com>
2024-08-04 16:39:53 -0700

    two-sum : first attempt completed

--- 1-two-sum/attempt-1.py
+++ 1-two-sum/attempt-1.py
@@ -1,2 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_enumerate = {}
+        for index, num in enumerate(nums):
+            nums_enumerate[num] = index
+        for index, num in enumerate(nums):
+            if (complement := target - num) in nums_enumerate and (second_index := nums_enumerate[complement]) != index:
+                return [index, second_index]
+
+        return []
